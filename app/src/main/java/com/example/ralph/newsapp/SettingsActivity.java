cc5ca77f0b3b16ef51957866e47f7f21d07e9d43
RalphM <36464444+lakerfan32@users.noreply.github.com>
2018-08-10 23:42:21 -0400

    -Created SettingsActivity and Preference screens -Added menu on the app bar -Setup Menu items

---
+++ app/src/main/java/com/example/ralph/newsapp/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.example.ralph.newsapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * This is used to contain a PreferenceFragment
+ */
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class NewsPreferenceFragment extends PreferenceFragment {
+
+    }
+}
